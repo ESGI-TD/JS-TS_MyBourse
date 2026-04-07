cd263842e68bec055b153314fde5290207f4c18c
KomaedaN <thomasmrt200@gmail.com>
2026-04-07 22:34:53 +0200

    feature(localStorage): add dark mode preference in local storage

--- src/ui/darkMode.ts
+++ src/ui/darkMode.ts
@@ -2,15 +2,20 @@ export function setDarkMode() {
   const sun = document.getElementById("sun") as HTMLElement;
   const moon = document.getElementById("moon") as HTMLElement;
   if (!sun || !moon) return;
+  const localDarkMode = localStorage.getItem("dark_mode");
+  if (localDarkMode === "moon") {
+  }
   sun.addEventListener("click", () => {
     sun.classList.toggle("hide");
     moon.classList.toggle("hide");
     document.body.classList.toggle("dark_mode");
+    localStorage.setItem("darkmode", "sun");
   });
 
   moon.addEventListener("click", () => {
     sun.classList.toggle("hide");
     moon.classList.toggle("hide");
     document.body.classList.toggle("dark_mode");
+    localStorage.setItem("darkmode", "moon");
   });
 }
